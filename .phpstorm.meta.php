c9f3f1a3bbf002ad5fb05d7be3891f5587ce0fa6
ivanov.dm <dmitriy.ivanov@brightpark.ru>
2020-01-10 19:46:25 +0500

    Added Auth

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-12-24 08:53:07.
+    * Generated on 2020-01-10 12:43:48.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.9.0 on 2019-12-24 08:53:06.
+ * Generated for Laravel 6.9.0 on 2020-01-10 12:43:48.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
