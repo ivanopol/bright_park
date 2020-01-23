5f5fd0dc8333eb2b8dd99ad11e79f6795913d6ef
ivanopol <ivanopol777@mail.ru>
2020-01-23 14:53:07 +0500

    Добавил обработку номера телефона

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@
 require('./bootstrap');
 window.Vue = require('vue');
 window.Vuex = require('vuex');
+
 //import Vuex from 'vuex';
 
 Vue.use(Vuex);
--- resources/js/components/FormComponent.vue
+++ resources/js/components/FormComponent.vue
@@ -6,18 +6,47 @@
         </div>
         <form action="#" id="form_test-drive" method="POST" name="feedback" @submit="send">
             <input type="text" class="" name="name" placeholder="Имя">
-            <input type="phone" class="" name="phone" placeholder="Телефон">
+            <input
+                type="tel"
+                v-model="phone"
+                name="phone"
+                id="phone"
+                placeholder="Телефон"
+                autocomplete="tel"
+                maxlength="14"
+                class="form-control"
+                v-phone
+                pattern="[(][0-9]{3}[)] [0-9]{3}-[0-9]{4}"
+                required
+            />
             <button :click="send">Записаться</button>
         </form>
     </section>
 </template>
 
 <script>
+
+    Vue.directive('phone', {
+        bind(el) {
+            el.oninput = function(e) {
+                if (!e.isTrusted) {
+                    return;
+                }
+
+                let x = this.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+                this.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+                el.dispatchEvent(new Event('input'));
+            }
+        }
+    });
+
     export default {
         name: 'App',
         props : [],
         data: function () {
-            return {};
+            return {
+                phone: ''
+            };
         },
         methods: {
             send(event) {
