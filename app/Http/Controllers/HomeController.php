1a9e2b66d575abae0dd5551f6fc63786826c0761
ivanopol <ivanopol777@mail.ru>
2020-01-23 10:37:25 +0500

    Добавил выделение для активного цвета

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -147,41 +147,49 @@ class HomeController extends Controller
                     'title' => 'Голубая планета',
                     'image' => 'build/images/colors/lada/granta/lada_granta_blue_planet.png',
                     'class' => 'color-blue-planet',
+                    'is_active' => true
                 ],
                 [
                     'title' => 'Сердолик',
                     'image' => 'build/images/colors/lada/granta/lada_granta_cornelian.png',
                     'class' => 'color-cornelian',
+                    'is_active' => false
                 ],
                 [
                     'title' => 'Белое облако',
                     'image' => 'build/images/colors/lada/granta/lada_granta_white_cloud.png',
                     'class' => 'color-white-cloud',
+                    'is_active' => false
                 ],
                 [
                     'title' => 'Пантера',
                     'image' => 'build/images/colors/lada/granta/lada_granta_panther.png',
                     'class' => 'color-panther',
+                    'is_active' => false
                 ],
                 [
                     'title' => 'Рислинг',
                     'image' => 'build/images/colors/lada/granta/lada_granta_riesling.png',
                     'class' => 'color-riesling',
+                    'is_active' => false
                 ],
                 [
                     'title' => 'Техно',
                     'image' => 'build/images/colors/lada/granta/lada_granta_techno.png',
                     'class' => 'color-techno',
+                    'is_active' => false
                 ],
                 [
                     'title' => 'Борнео',
                     'image' => 'build/images/colors/lada/granta/lada_granta_borneo.png',
                     'class' => 'color-borneo',
+                    'is_active' => false
                 ],
                 [
                     'title' => 'Кориандр',
                     'image' => 'build/images/colors/lada/granta/lada_granta_coriander.png',
                     'class' => 'color-coriander',
+                    'is_active' => false
                 ],
             ],
         ];
--- resources/js/components/ColorChooseComponent.vue
+++ resources/js/components/ColorChooseComponent.vue
@@ -10,8 +10,8 @@
         <div class="palette">
             <div class="color-name" v-text="title"></div>
             <ul>
-                <li v-for="(color, id) in colors" >
-                    <div :class="'circle ' + color.class" :data-name="color.title" :data-image="color.image" @click="change_color(color)"></div>
+                <li v-for="(color, id) in colors" :class="{ active : color.is_active }">
+                    <div class="circle" :class="color.class" :data-name="color.title" :data-image="color.image" @click="change_color(color, $event)"></div>
                 </li>
             </ul>
         </div>
@@ -29,9 +29,13 @@
             };
         },
         methods: {
-            change_color(color) {
+            change_color(color, e) {
                 this.image = color.image;
                 this.title = color.title;
+                this.colors.forEach(function(item, i, arr) {
+                    item.is_active = false;
+                });
+                color.is_active = true;
             }
         }
     }
