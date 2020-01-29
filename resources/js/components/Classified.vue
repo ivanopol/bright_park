2d624b739165ebf53f9f338c5e322cbafa85d0b0
ivanopol <ivanopol777@mail.ru>
2020-01-29 16:16:57 +0500

    Добавил контент в компонент классификатора

--- resources/js/components/Classified.vue
+++ resources/js/components/Classified.vue
@@ -1,6 +1,43 @@
 <template>
-    <section id="classifide">
+    <section id="classified">
+        <div class="dropdown-group-title">Оцените свой автомобиль</div>
+        <div class="container dropdown-group">
+            <select class="dropdown">
+                <option selected="selected">Марка</option>
+            </select>
+            <select>
+                <option selected="selected">Модель</option>
+            </select>
+            <select>
+                <option selected="selected">Год выпуска</option>
+            </select>
+            <select>
+                <option selected="selected">Комплектация</option>
+            </select>
+            <select>
+                <option selected="selected">Пробег</option>
+            </select>
+        </div>
 
+        <div class="trigger-wrap">
+            <p class="trigger-wrap-text">
+                Рыночная оценка <span class="model-count-text">666 руб</span> <br/>
+                Оценка Bright Park <span class="model-count-text">666 руб</span>
+            </p>
+        </div>
+
+        <div class="model-choose-text">
+            <p>Предложение действует до 16 января</p>
+        </div>
+
+        <div class="button-wrapper-row">
+            <button class="hollow-button-halfsize hollow-button-recolored">
+                <a href='/trade_in_cash'>Наличный расчет</a>
+            </button>
+            <button class="hollow-button-halfsize hollow-button-recolored">
+                <a href="/trade_in_credit">В кредит</a>
+            </button>
+        </div>
     </section>
 </template>
 
