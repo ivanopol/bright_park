fa1638cd7e8e22fb744542ee7d59e18943a51af1
ivanopol <ivanopol777@mail.ru>
2020-01-29 16:16:33 +0500

    Отключил Bootstrap и JQuery из загрузки

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+//require('./bootstrap');
 window.Vue = require('vue');
 window.Vuex = require('vuex');
 //import Vuex from 'vuex';
