0f19f8149f3495b2343967f3d922675c22b6c393
ivanopol <ivanopol777@mail.ru>
2020-02-04 13:25:01 +0500

    Добавил модели для моделей машины и кузовов

---
+++ app/CarModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CarModel extends Model
+{
+    /**
+     * Таблица, связанная с моделью.
+     *
+     * @var string
+     */
+    protected $table = 'car_models';
+}
---
+++ app/CarType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CarType extends Model
+{
+    /**
+     * Таблица, связанная с моделью.
+     *
+     * @var string
+     */
+    protected $table = 'car_types';
+}
