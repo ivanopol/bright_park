1c1e53630ab22d69ae0235c17e36bfc1f6aee884
ivanopol <ivanopol777@mail.ru>
2020-02-05 18:06:08 +0500

    Модели: Добавил новые картинки слайдеров

--- app/Services/BasePageService.php
+++ app/Services/BasePageService.php
@@ -15,10 +15,8 @@ class BasePageService
             ['type_id', '=', $car_type->id],
         ];
 
+        // Получаем информацию по блокам
         $blocks_ids = [];
-
-        $reviews = DB::table('reviews')->select('*')->where($condition)->get();
-        $colors = DB::table('colors')->select('*')->where($condition)->get();
         $blocks = DB::table('blocks')->select('*')->where($condition)->get();
 
         foreach ($blocks as $block) {
@@ -26,11 +24,6 @@ class BasePageService
         }
 
         $blocks_slider = DB::table('block_sliders')->select('*')->whereIn('block_id', $blocks_ids)->orderBy('id', 'asc')->get();
-        $slides = DB::table('slides')->select('*')->where($condition)->get();
-        $slide_mini = DB::table('slide_mini')->select('*')->where([
-            ['model_id', '=', $car_model->id]
-        ])->get();
-
         foreach ($blocks as &$block) {
             $block->slider = [];
 
@@ -40,6 +33,16 @@ class BasePageService
                 }
             }
         }
+
+        // Получаем информацию по слайдам
+        $slides = DB::table('slides')->select('*')->where($condition)->get();
+        $slide_mini = DB::table('slide_mini')->select('*')->where([
+            ['model_id', '=', $car_model->id]
+        ])->get();
+
+        // Получаем информацию по отзывам и по цветам
+        $reviews = DB::table('reviews')->select('*')->where($condition)->get();
+        $colors = DB::table('colors')->select('*')->where($condition)->get();
 
         $data = [
             'slider' => [
