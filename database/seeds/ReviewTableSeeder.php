8fe541566a4296c3887955a51dae15fa53b843db
ivanopol <ivanopol777@mail.ru>
2020-02-07 15:24:26 +0500

    Добавил отзывы по 4х4

--- database/seeds/ReviewTableSeeder.php
+++ database/seeds/ReviewTableSeeder.php
@@ -49,6 +49,27 @@ class ReviewTableSeeder extends Seeder
                 'url' => 'https://www.kolesa.ru/article/pyat-veshhej-za-kotorye-lyubyat-i-nenavidyat-lada-granta',
                 'quote' => 'Среди покупателей этой машины до сих пор много тех, кто и техобслуживание, и ремонт ведёт своими силами, в гараже.',
                 'author' => 'портал <cite>КОЛЕСА.ру</cite>'
+            ],
+            [
+                'model_id' => 4,
+                'type_id' => 11,
+                'url' => 'https://www.kolesa.ru/article/pyat-veshhej-za-kotorye-lyubyat-i-nenavidyat-lada-granta',
+                'quote' => 'Гранта сделана в России и специально для России. Клиренс в 165 мм хвалят очень многие владельцы этого авто',
+                'author' => 'портал <cite>КОЛЕСА.ру</cite>'
+            ],
+            [
+                'model_id' => 4,
+                'type_id' => 11,
+                'url' => 'https://www.kolesa.ru/article/pyat-veshhej-za-kotorye-lyubyat-i-nenavidyat-lada-granta',
+                'quote' => 'Обновлённая Гранта предлагается с четырьмя типами кузова: кроме традиционного седана, это лифтбек, хэтчбек и даже универсал! ',
+                'author' => 'портал <cite>КОЛЕСА.ру</cite>'
+            ],
+            [
+                'model_id' => 4,
+                'type_id' => 11,
+                'url' => 'https://www.kolesa.ru/article/pyat-veshhej-za-kotorye-lyubyat-i-nenavidyat-lada-granta',
+                'quote' => 'Среди покупателей этой машины до сих пор много тех, кто и техобслуживание, и ремонт ведёт своими силами, в гараже.',
+                'author' => 'портал <cite>КОЛЕСА.ру</cite>'
             ]
         ];
 
