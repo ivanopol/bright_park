8ed8db1c0a6822fd05ca85626b34aa1308738bef
ivanopol <ivanopol777@mail.ru>
2020-02-08 14:40:09 +0500

    Обновил composer.json
    Для того, чтобы заработал modelsTableSeeds

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-02-04 06:30:54.
+    * Generated on 2020-02-08 07:14:13.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
