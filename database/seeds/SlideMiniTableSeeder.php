500d1470a56c7f7f17f56e6200a614e497debc70
ivanopol <ivanopol777@mail.ru>
2020-02-08 16:16:27 +0500

    Исправил ID гранты хэтчбека

--- database/seeds/SlideMiniTableSeeder.php
+++ database/seeds/SlideMiniTableSeeder.php
@@ -17,35 +17,35 @@ class SlideMiniTableSeeder extends Seeder
             ],
             [
                 'model_id' => 1,
-                'type_id' => 3,
+                'type_id' => 2,
                 'image' => '/build/images/slider/mini/granta/mobile/lada_granta_liftback.png',
                 'alt' =>  'Лада Гранта лифтбек',
                 'title' => 'Лифтбек'
             ],
             [
                 'model_id' => 1,
-                'type_id' => 4,
+                'type_id' => 3,
                 'image' => '/build/images/slider/mini/granta/mobile/lada_granta_universal.png',
                 'alt' => 'Лада Гранта универсал',
                 'title' => 'Универсал'
             ],
             [
                 'model_id' => 1,
-                'type_id' => 5,
+                'type_id' => 4,
                 'image' => '/build/images/slider/mini/granta/mobile/lada_granta_cross.png',
                 'alt'=> 'Лада Гранта cross',
                 'title' => 'Cross'
             ],
             [
                 'model_id' => 1,
-                'type_id' => 6,
+                'type_id' => 5,
                 'image' => '/build/images/slider/mini/granta/mobile/lada_granta_drive_active.png',
                 'alt' => 'Лада Гранта Drive Active',
                 'title' => 'Drive Active'
             ],
             [
                 'model_id' => 1,
-                'type_id' => 1,
+                'type_id' =>6,
                 'image' => '/build/images/slider/mini/granta/mobile/lada_granta_hatcback.png',
                 'alt' => 'Лада Гранта хэтчбек',
                 'title' => 'Хэтчбек'
