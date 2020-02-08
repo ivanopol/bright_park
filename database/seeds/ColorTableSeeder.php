d39a3282d183414a866b1eb9ad9496227002f5d6
ivanopol <ivanopol777@mail.ru>
2020-02-08 16:12:58 +0500

    Исправил связь блоков в плюсах со слайдами

--- database/seeds/ColorTableSeeder.php
+++ database/seeds/ColorTableSeeder.php
@@ -219,7 +219,7 @@ class ColorTableSeeder extends Seeder
                 'type_id' => 20,
                 'model_id' => 3,
                 'title' => 'Ледниковый',
-                'image' => '/build/images/colors/lada/xray/lada_xray_glacial.png',
+                'image' => '/build/images/colors/lada/xray/xray/lada_xray_glacial.png',
                 'class' => 'color-white-glacial',
                 'is_active' => true
             ],
@@ -227,7 +227,7 @@ class ColorTableSeeder extends Seeder
                 'type_id' => 20,
                 'model_id' => 3,
                 'title' => 'Огненно-красный',
-                'image' => '/build/images/colors/lada/xray/lada_xray_fiery_red.png',
+                'image' => '/build/images/colors/lada/xray/xray/lada_xray_fiery_red.png',
                 'class' => 'color-fiery-red',
                 'is_active' => false
             ],
@@ -235,7 +235,7 @@ class ColorTableSeeder extends Seeder
                 'type_id' => 20,
                 'model_id' => 3,
                 'title' => 'Серый базальт',
-                'image' => '/build/images/colors/lada/xray/lada_xray_basalt_gray.png',
+                'image' => '/build/images/colors/lada/xray/xray/lada_xray_basalt_gray.png',
                 'class' => 'color-basalt-gray',
                 'is_active' => false
             ],
@@ -243,7 +243,7 @@ class ColorTableSeeder extends Seeder
                 'type_id' => 20,
                 'model_id' => 3,
                 'title' => 'Пума',
-                'image' => '/build/images/colors/lada/xray/lada_xray_light_brown_cougar.png',
+                'image' => '/build/images/colors/lada/xray/xray/lada_xray_light_brown_cougar.png',
                 'class' => 'color-light-brown-cougar',
                 'is_active' => false
             ],
@@ -251,7 +251,7 @@ class ColorTableSeeder extends Seeder
                 'type_id' => 20,
                 'model_id' => 3,
                 'title' => 'Черная жемчужина',
-                'image' => '/build/images/colors/lada/xray/lada_xray_black_pearl.png',
+                'image' => '/build/images/colors/lada/xray/xray/lada_xray_black_pearl.png',
                 'class' => 'color-black-pearl',
                 'is_active' => false
             ],
@@ -259,7 +259,7 @@ class ColorTableSeeder extends Seeder
                 'type_id' => 20,
                 'model_id' => 3,
                 'title' => 'Платина',
-                'image' => '/build/images/colors/lada/xray/lada_xray_platina.png',
+                'image' => '/build/images/colors/lada/xray/xray/lada_xray_platina.png',
                 'class' => 'color-platina',
                 'is_active' => false
             ],
