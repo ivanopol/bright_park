7d33c6316152c6b9497feb25008628c6675999de
ivanopol <ivanopol777@mail.ru>
2020-02-14 13:40:05 +0500

    Добавил бесконечную прокрутку для слайдера в моделях

--- resources/js/components/HooperComponent.vue
+++ resources/js/components/HooperComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="carcasses-wrap">
         <div class="items-mini_wrap">
-            <hooper class="items-mini" :itemsToShow="3" :centerMode="true" v-on:slide="change_line">
+            <hooper class="items-mini" :mouseDrag="false" :infiniteScroll="true" :itemsToShow="3" :centerMode="true" v-on:slide="change_line">
                 <slide v-for="slide in data.slides_mini" v-bind:key="slide.id">
                     <a :href="slide.url">
                         <img :src="slide.image" :alt="slide.alt">
