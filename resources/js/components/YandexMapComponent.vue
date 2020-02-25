2f34b6d47f61705e4931a95911d077b7db8410e6
Armen <keith.waren@gmail.com>
2020-02-25 13:49:07 +0500

    geolocation rework

--- resources/js/components/YandexMapComponent.vue
+++ resources/js/components/YandexMapComponent.vue
@@ -23,60 +23,61 @@
         methods: {
             createRoute () {
 
-                let _self = this;
+                if(!this.routeExist){
+                    let _self = this;
 
-                function success(position){
-                    const latitude = position.coords.latitude;
-                    const longitude = position.coords.longitude;
+                    function success(position){
+                        const latitude = position.coords.latitude;
+                        const longitude = position.coords.longitude;
 
-                    addUserLocation([latitude, longitude])
-                }
+                        addUserLocation([latitude, longitude])
+                    }
 
-                async function error(e) {
+                    async function error(e) {
 
-                    const res = await fetch('https://location.services.mozilla.com/v1/geolocate?key=test').then(el=>el.json());
-                    const point = [res.location.lat, res.location.lng];
+                        const res = await fetch('https://location.services.mozilla.com/v1/geolocate?key=test').then(el=>el.json());
+                        const point = [res.location.lat, res.location.lng];
 
-                    addUserLocation(point);
+                        addUserLocation(point);
 
-                    console.log('Unable to retrieve your location');
-                    console.log(e)
-                }
+                        console.log(e)
+                    }
 
-                if (!navigator.geolocation) {
-                    console.log('Geolocation is not supported by your browser');
-                } else {
-                    navigator.geolocation.getCurrentPosition(success, error);
-                }
+                    if (!navigator.geolocation) {
+                        console.log('Geolocation is not supported by your browser');
+                    } else {
+                        navigator.geolocation.getCurrentPosition(success, error);
+                    }
 
-                function addUserLocation(coords) {
-                    let userLocation = new ymaps.GeoObject({
-                        // Описание геометрии.
-                        geometry: {
-                            type: "Point",
-                            coordinates: coords
-                        },
-                    }, {
-                        draggable: true
-                    });
+                    function addUserLocation(coords) {
+                        let userLocation = new ymaps.GeoObject({
+                            // Описание геометрии.
+                            geometry: {
+                                type: "Point",
+                                coordinates: coords
+                            },
+                        }, {
+                            draggable: true
+                        });
 
-                    let Route = new ymaps.multiRouter.MultiRoute({
-                            referencePoints: [userLocation,
-                                _self.brightParkLocation
-                            ],
-                            params: {
-                                routingMode: 'driving',
-                                reverseGeocoding: true
+                        let Route = new ymaps.multiRouter.MultiRoute({
+                                referencePoints: [userLocation,
+                                    _self.brightParkLocation
+                                ],
+                                params: {
+                                    routingMode: 'driving',
+                                    reverseGeocoding: true
+                                }
+                            },
+                            {
+                                boundsAutoApply: true
                             }
-                        },
-                        {
-                            boundsAutoApply: true
-                        }
-                    );
+                        );
 
-                    _self.map.geoObjects.add(Route);
-                    _self.map.geoObjects.remove(_self.brightParkLocation);
-                    _self.routeExist = true;
+                        _self.map.geoObjects.add(Route);
+                        _self.map.geoObjects.remove(_self.brightParkLocation);
+                        _self.routeExist = true;
+                    }
                 }
             }
         },
@@ -87,7 +88,7 @@
                     myMap = new ymaps.Map('map', {
                         center: this.coordinates,
                         zoom: 15,
-                        controls: ['zoomControl']
+                        controls: ['geolocationControl', 'zoomControl']
                     }, {
                         searchControlProvider: 'yandex#search'
                     }),
