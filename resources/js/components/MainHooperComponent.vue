46f84f81b211e63aa5b1eff5d709ef4c6b858fb9
ivanopol <ivanopol777@mail.ru>
2020-02-25 12:49:59 +0500

    Изменения в верстке слайдера на главной странице

--- resources/js/components/MainHooperComponent.vue
+++ resources/js/components/MainHooperComponent.vue
@@ -108,7 +108,7 @@
     }
 
     .main-screen-wrap.hooper {
-        background-color: #000;
+       /* background-color: #000;*/
         top: -60px;
     }
 
@@ -134,11 +134,11 @@
 
         .asset-container {
             /*background-color: #000;*/
-            height: 100vh;
+            height: 100%;
         }
         .asset-image {
             object-fit: cover;
-            margin: 0;
+            margin: -75px 0 0 0;
             width: 100vw;
             //position: absolute;
             //bottom: 0;
@@ -146,10 +146,10 @@
 
         .product-content {
             position: absolute;
-            top: 15vh;
+            top: 5%;
             color: white;
             text-align: center;
-            height: 75vh;
+            height: 78%;
 
             .title-model {
                 font-family: PragmaticaLightCBold, Helvetica, sans-serif;
@@ -160,10 +160,10 @@
             }
 
             .title-tagline {
-                font-size: 150%;
+                font-size: 26px;
                 //font-weight: bold;
                 width: 78vw;
-                margin: 40px auto 0;
+                margin: 30px auto 0;
                 line-height: 1.2;
 /*                -webkit-transition: all ease-out 0.5s;
                 -moz-transition: all ease-out 0.5s;
@@ -173,7 +173,7 @@
 
                 &.bold {
                     font-family: PragmaticaLightCBold, Helvetica, sans-serif;
-                    font-size: 170%;
+                    font-size: 26px;
                 }
 
                 .highlight {
