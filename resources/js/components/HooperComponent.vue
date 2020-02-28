559729bfa72c9db3128d4777b2359368cdf56d21
ivanopol <ivanopol777@mail.ru>
2020-02-28 13:32:52 +0500

    Добавил перестроение слайдера миниатюр для старницы моделей

--- resources/js/components/HooperComponent.vue
+++ resources/js/components/HooperComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="carcasses-wrap">
         <div class="items-mini_wrap">
-            <hooper class="items-mini" :centerMode="centerMode" :mouseDrag="false" :infiniteScroll="infinite" :itemsToShow="itemsToShow" :trimWhiteSpace="trimWhiteSpace"  v-on:slide="change_line">
+            <hooper class="items-mini" :settings="hooperSettings" v-on:slide="change_line">
                 <slide v-for="slide in data.slides_mini" v-bind:key="slide.id">
                     <a :href="slide.url">
                         <img :src="slide.image" :alt="slide.alt">
@@ -22,52 +22,50 @@
                 </span>
             </div>
         </div>
-        <resize-observer @notify="handleResize" />
     </div>
 </template>
 
 <script>
     import { Hooper, Slide } from 'hooper';
     import 'hooper/dist/hooper.css';
-    import { ResizeObserver } from 'vue-resize'
 
     export default {
         name: 'App',
         components: {
             Hooper,
             Slide,
-            ResizeObserver
         },
         props : ['data'],
-        data: function () {
+        data () {
             return {
-                infinite: this.data.slides_mini.length > 2,
-                itemsToShow: 3,
-                centerMode: true,
-                trimWhiteSpace: false
+                hooperSettings: {
+                    infiniteScroll: this.data.slides_mini.length > 2,
+                    itemsToShow: 3,
+                    centerMode: true,
+                    trimWhiteSpace: false,
+                    mouseDrag: false,
+                    breakpoints: {
+                        1366: {
+                            infiniteScroll: false,
+                            itemsToShow: 6,
+                            centerMode: false,
+                            trimWhiteSpace: true,
+                            mouseDrag: true,
+                        }
+                    }
+                }
             };
         },
         mounted: function() {
          this.$children[0].slideTo(this.data.active);
         },
         methods: {
-            handleResize () {
-                console.log('resized')
-            },
             change_line: function(event) {
             },
             elem() {
                 this.size = window.innerWidth;
                 return this.size;
             },
-        },
-        created: function() {
-            if (window.innerWidth > 1366) {
-                this.itemsToShow = 6;
-                this.infinite = false;
-                this.centerMode = false;
-                this.trimWhiteSpace = true;
-            }
         }
     };
 </script>
