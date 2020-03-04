50bfd7fc8da4398b7590db21494e561bcb8601ef
ivanopol <ivanopol777@mail.ru>
2020-03-04 16:39:53 +0500

    Добавил позиционирование индикатора для десктопов

--- resources/js/components/HooperComponent.vue
+++ resources/js/components/HooperComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="carcasses-wrap">
         <div class="items-mini_wrap container">
-            <hooper class="items-mini" :settings="hooperSettings" v-on:slide="change_line">
+            <hooper class="items-mini" id="items-mini" :settings="hooperSettings" v-on:slide="change_line">
                 <slide v-for="slide in data.slides_mini" v-bind:key="slide.id">
-                    <a :href="slide.url">
+                    <a :href="slide.url" :class="{ current_model : slide.active}">
                         <picture>
                             <source :srcset="get_source(slide.image)" media="(min-width: 580px)">
                             <img :src="slide.image" :alt="slide.alt">
@@ -11,6 +11,9 @@
                         <span v-text="slide.title">Седан</span>
                     </a>
                 </slide>
+                <!--              <hooper-navigation slot="hooper-addons"></hooper-navigation>
+                              <hooper-pagination slot="hooper-addons"></hooper-pagination>
+                              <hooper-progress slot="hooper-addons"></hooper-progress>-->
             </hooper>
             <span class="indicator"></span>
         </div>
@@ -29,14 +32,24 @@
 </template>
 
 <script>
-    import { Hooper, Slide } from 'hooper';
+    import {
+        Hooper,
+        Slide
+/*        Progress as HooperProgress,
+        Pagination as HooperPagination,
+        Navigation as HooperNavigation*/
+
+    } from 'hooper';
     import 'hooper/dist/hooper.css';
 
     export default {
         name: 'App',
         components: {
             Hooper,
-            Slide,
+            Slide
+/*            HooperProgress,
+            HooperPagination,
+            HooperNavigation*/
         },
         props : ['data'],
         data () {
@@ -55,13 +68,27 @@
                             centerMode: false,
                             trimWhiteSpace: true,
                             mouseDrag: true,
-                        }
+                        },
+                        800: {
+                            infiniteScroll: false,
+                            itemsToShow: 6,
+                            centerMode: false,
+                            trimWhiteSpace: true,
+                            mouseDrag: true
+                        },
                     }
                 }
             };
         },
         mounted: function() {
-            this.$children[0].slideTo(this.data.active);
+            if (window.innerWidth < 581) {
+                this.$children[0].slideTo(this.data.active);
+                this.setIndicatorDefaultPosition();
+            } else {
+                setTimeout(() => this.setIndicatorPosition(), 300);
+            }
+
+            window.addEventListener("resize", this.myEventHandler);
         },
         methods: {
             get_source: function(path) {
@@ -70,10 +97,29 @@
             },
             change_line: function(event) {
             },
-            elem() {
-                this.size = window.innerWidth;
-                return this.size;
+            myEventHandler: function() {
+                if (window.innerWidth < 581) {
+                    this.$children[0].slideTo(this.data.active);
+                    this.setIndicatorDefaultPosition();
+                } else {
+                    this.$children[0].slideTo(0);
+                    this.setIndicatorPosition();
+                }
             },
+            setIndicatorPosition: function() {
+                var model = document.getElementById('items-mini').getElementsByClassName('current_model')[0].getBoundingClientRect();
+                var parent = document.getElementById('items-mini').getBoundingClientRect();
+                var pos = model.left - parent.left + model.width / 2;
+
+                if (pos) {
+                    var indicator = document.getElementsByClassName('indicator')[0];
+                    indicator.style.left = pos + 'px';
+                }
+            },
+            setIndicatorDefaultPosition: function() {
+                var indicator = document.getElementsByClassName('indicator')[0];
+                indicator.style.left = '48%';
+            }
         }
     };
 </script>
