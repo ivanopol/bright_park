28bf8644fd1f41fa4d3ecb61bd0eb394b070059b
ivanopol <ivanopol777@mail.ru>
2020-03-05 20:49:55 +0500

    Добавил стили для блока отзывов

--- resources/js/components/ReviewComponent.vue
+++ resources/js/components/ReviewComponent.vue
@@ -1,9 +1,9 @@
 <template>
-    <section class="block block_4">
+    <section class="block block_4 container">
         <div class="block-text">
             <h2><span class="c_orange">Эксперты</span> о {{model_name}}</h2>
         </div>
-        <div class="opinions">
+        <div class="opinions-wrap">
             <hooper class="opinions">
                 <slide v-for="review in reviews" :key="reviews.key">
                     <blockquote :cite="review.url">
