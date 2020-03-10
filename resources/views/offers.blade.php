12e13ad9836538b7f899a1d3058f1b970cc1b21c
ivanopol <ivanopol777@mail.ru>
2020-03-10 13:45:29 +0500

    Ретаргетинговая страница: оформил блок преимуществ

--- resources/views/offers.blade.php
+++ resources/views/offers.blade.php
@@ -9,30 +9,30 @@
     </section>
     <body>
     <div class="container">
-        <div id="advantages" class="advantages_wrap">
+        <div id="advantages" class="advantages_wrap retargeting_wrap">
             <h1><span class="c_orange">Преимущества</span> покупки сейчас</h1>
             <ul>
                 <li>
                     <div class="advantage">
-                        <span class="value"></span>
+                        <span class="icon"><img src="/build/images/icons/icon-road.svg" alt="Брайт Парк: дорога до салона за наш счет"></span>
                         <span class="text">Дорога до салона за наш счет</span>
                     </div>
                 </li>
                 <li>
                     <div class="advantage">
-                        <span class="value"></span>
+                        <span class="icon"><img src="/build/images/icons/icon-percent.svg" alt="Брайт Парк: подберем выгодную ставку до 3%"></span>
                         <span class="text">Подберем выгодную ставку до 3%</span>
                     </div>
                 </li>
                 <li>
                     <div class="advantage">
-                        <span class="value"></span>
+                        <span class="icon"><img src="/build/images/icons/icon-gift.svg" alt="Брайт Парк: КАСКО в подарок"></span>
                         <span class="text">КАСКО в подарок</span>
                     </div>
                 </li>
                 <li>
                     <div class="advantage">
-                        <span class="value"></span>
+                        <span class="icon"><img src="/build/images/icons/icon-complect.svg" alt="Брайт Парк: зимняя резина в комплекте"></span>
                         <span class="text">Зимняя резина в комплекте</span>
                     </div>
                 </li>
