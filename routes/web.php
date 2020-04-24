d085fb70f4f8d7676db980735f98230df9690259
ivanopol <ivanopol777@mail.ru>
2020-04-24 20:53:04 +0500

    Добавил редиректы по Перми

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-https://brightpark.ru/volgograd/
+
 //Auth::routes();
+
+// Редиректы
+// Ретаргетинг и прочее
 Route::get('/perm/lada-v-brajt-parke-na-osobyh-usloviyah/', function () { return redirect('/perm/special_offers/'); });
 Route::get('/moscow/lada-v-brajt-parke-na-osobyh-usloviyah/', function () { return redirect('/moscow/special_offers/'); });
 Route::get('/yekaterinburg/lada-v-brajt-parke-na-osobyh-usloviyah/', function () { return redirect('/yekaterinburg/special_offers/'); });
 Route::get('/volgograd/lada-v-brajt-parke-na-osobyh-usloviyah/', function () { return redirect('/volgograd/special_offers/'); });
 Route::get('/magnitogorsk/lada-v-brajt-parke-na-osobyh-usloviyah/', function () { return redirect('/magnitogorsk/special_offers/'); });
 Route::get('/rostov-na-donu/lada-v-brajt-parke-na-osobyh-usloviyah/', function () { return redirect('/rostov-na-donu/special_offers/'); });
+Route::get('/perm/services/', function () { return redirect('/perm/stocks/'); });
+Route::get('/perm/services/maksimalnaya-vygoda-po-gosprogrammam-pervyjsemejnyj-avtomobil/', function () { return redirect('/perm/stocks/lada_vesta_vygoda_10_po_gosprogramme'); });
+Route::get('/perm/ocenka-avtomobilya/', function () { return redirect('/perm/stocks/doplata_40_000_rublej_pri_obmene_na_lada_granta/'); });
+Route::get('/perm/raschyot-kredita/', function () { return redirect('/perm/stocks/lada_granta_0_pervonachalnyj_vznos/'); });
 
+// Granta
 Route::get('/perm/catalog/lada-granta/', function () { return redirect('/perm/granta/sedan/'); });
 Route::get('/perm/catalog/lada-granta/granta-sedan/', function () { return redirect('/perm/granta/sedan/'); });
 Route::get('/perm/catalog/lada-granta/granta-liftbek/', function () { return redirect('/perm/granta/liftback/'); });
@@ -26,6 +34,45 @@ Route::get('/perm/catalog/lada-granta/granta-hetchbek/', function () { return re
 Route::get('/perm/catalog/lada-granta/granta-universal/', function () { return redirect('/perm/granta/sedan/'); });
 Route::get('/perm/catalog/lada-granta/granta-cross/', function () { return redirect('/perm/granta/cross/'); });
 Route::get('/perm/catalog/lada-granta/granta-drive-active/', function () { return redirect('/perm/granta/drive-active/'); });
+
+// Vesta
+Route::get('/perm/catalog/lada-vesta/', function () { return redirect('/perm/vesta/sedan/'); });
+Route::get('/perm/catalog/catalog/lada-vesta/vesta-sedan/', function () { return redirect('/perm/vesta/sedan/'); });
+Route::get('/perm/catalog/lada-vesta/vesta-cross/', function () { return redirect('/perm/vesta/sedan/'); });
+Route::get('/perm/catalog/catalog/lada-vesta/vesta-sw/', function () { return redirect('/perm/vesta/sw/'); });
+Route::get('/perm/catalog/lada-vesta/vesta-sw-cross/', function () { return redirect('/perm/vesta/sw-cross/'); });
+Route::get('/perm/catalog/lada-vesta/vesta-cng/', function () { return redirect('/perm/vesta/sedan/'); });
+Route::get('/perm/catalog/lada-vesta/vesta-sport/', function () { return redirect('/perm/vesta/sedan/'); });
+
+// XRAY
+Route::get('/perm/catalog/lada-xray/xray/', function () { return redirect('/perm/xray/xray/'); });
+Route::get('/perm/catalog/lada-xray/xray-cross/', function () { return redirect('/perm/xray/cross/'); });
+
+// Largus
+Route::get('/perm/catalog/lada-largus/', function () { return redirect('/perm/largus/universal/'); });
+Route::get('/perm/catalog/lada-largus/largus-universal/', function () { return redirect('/perm/largus/universal/'); });
+Route::get('/perm/catalog/lada-largus/largus-universal-cng/', function () { return redirect('/perm/largus/universal/'); });
+Route::get('/perm/catalog/lada-largus/largus-cross/', function () { return redirect('/perm/largus/universal/'); });
+Route::get('/perm/catalog/lada-largus/largus-cross-cng/', function () { return redirect('/perm/largus/universal/'); });
+Route::get('/perm/catalog/lada-largus/largus-furgon/', function () { return redirect('/perm/largus/universal/'); });
+Route::get('/perm/catalog/lada-largus/largus-furgon-cng/', function () { return redirect('/perm/largus/universal/'); });
+
+// Largus
+Route::get('/perm/catalog/lada-largus/', function () { return redirect('/perm/largus/universal/'); });
+Route::get('/perm/catalog/lada-largus/largus-universal/', function () { return redirect('/perm/largus/universal/'); });
+Route::get('/perm/catalog/lada-largus/largus-universal-cng/', function () { return redirect('/perm/largus/universal/'); });
+Route::get('/perm/catalog/lada-largus/largus-cross/', function () { return redirect('/perm/largus/universal/'); });
+Route::get('/perm/catalog/lada-largus/largus-cross-cng/', function () { return redirect('/perm/largus/universal/'); });
+Route::get('/perm/catalog/lada-largus/largus-furgon/', function () { return redirect('/perm/largus/universal/'); });
+Route::get('/perm/catalog/lada-largus/largus-furgon-cng/', function () { return redirect('/perm/largus/universal/'); });
+
+// 4x4
+Route::get('/perm/catalog/lada-4x4/', function () { return redirect('/perm/4x4/three-doors/'); });
+Route::get('/perm/catalog/lada-4x4/4x4-3-dv/', function () { return redirect('/perm/4x4/three-doors/'); });
+Route::get('/perm/catalog/lada-4x4/4x4-urban-3-dv/', function () { return redirect('/perm/4x4/three-doors/'); });
+Route::get('/perm/catalog/lada-4x4/4x4-5-dv/', function () { return redirect('/perm/4x4/three-doors/'); });
+Route::get('/perm/catalog/lada-4x4/4x4-urban-5-dv/', function () { return redirect('/perm/4x4/three-doors/'); });
+Route::get('/perm/catalog/lada-4x4/4x4-bronto/', function () { return redirect('/perm/4x4/three-doors/'); });
 
 
 Route::middleware(['cookie.check', 'counter'])->group(function () {
