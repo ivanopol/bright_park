aa26649655be1c61ef6146e8430869d2a76e0a2d
ivanopol <ivanopol777@mail.ru>
2020-05-18 16:04:40 +0500

    #service Добавил оформление аккордеона

--- resources/js/components/service/OurServices.vue
+++ resources/js/components/service/OurServices.vue
@@ -2,16 +2,16 @@
     <section class="car-services container">
         <h2><span class="c_orange">Наши</span> услуги</h2>
         <div class="container">
-            <badger-accordion>
+            <badger-accordion >
                 <badger-accordion-item>
                     <template slot="header">Техническое обслуживание (ТО)</template>
                     <template slot="content">
                         <ul>
-                            <li>Кузовной ремонт - от 500 руб.</li>
-                            <li>Локальный ремонт - от 3500 руб.</li>
-                            <li>Ремонт без окраса - от 1500 руб.</li>
-                            <li>Обработка арок и днища автомобиля антишумом - от 2500 руб.</li>
-                            <li>Нанесение защитной пленки - от 1500 руб.</li>
+                            <li><check-icon></check-icon> Кузовной ремонт - от&nbsp;500&nbsp;руб.</li>
+                            <li><check-icon></check-icon> Локальный ремонт - от&nbsp;3500&nbsp;руб.</li>
+                            <li><check-icon></check-icon> Ремонт без окраса - от&nbsp;1500&nbsp;руб.</li>
+                            <li><check-icon></check-icon> Обработка арок и днища автомобиля антишумом - от&nbsp;2500&nbsp;руб.</li>
+                            <li><check-icon></check-icon> Нанесение защитной пленки - от&nbsp;1500&nbsp;руб.</li>
                         </ul>
                     </template>
                 </badger-accordion-item>
@@ -20,11 +20,11 @@
                     <template slot="header">Кузовной ремонт</template>
                     <template slot="content">
                         <ul>
-                            <li>Кузовной ремонт - от 500 руб.</li>
-                            <li>Локальный ремонт - от 3500 руб.</li>
-                            <li>Ремонт без окраса - от 1500 руб.</li>
-                            <li>Обработка арок и днища автомобиля антишумом - от 2500 руб.</li>
-                            <li>Нанесение защитной пленки - от 1500 руб.</li>
+                            <li><check-icon></check-icon> Кузовной ремонт - от&nbsp;500&nbsp;руб.</li>
+                            <li><check-icon></check-icon> Локальный ремонт - от&nbsp;3500&nbsp;руб.</li>
+                            <li><check-icon></check-icon> Ремонт без окраса - от&nbsp;1500&nbsp;руб.</li>
+                            <li><check-icon></check-icon> Обработка арок и днища автомобиля антишумом - от&nbsp;2500&nbsp;руб.</li>
+                            <li><check-icon></check-icon> Нанесение защитной пленки - от&nbsp;1500&nbsp;руб.</li>
                         </ul>
                     </template>
                 </badger-accordion-item>
@@ -33,11 +33,11 @@
                     <template slot="header">Слесарный ремонт</template>
                     <template slot="content">
                         <ul>
-                            <li>Кузовной ремонт - от 500 руб.</li>
-                            <li>Локальный ремонт - от 3500 руб.</li>
-                            <li>Ремонт без окраса - от 1500 руб.</li>
-                            <li>Обработка арок и днища автомобиля антишумом - от 2500 руб.</li>
-                            <li>Нанесение защитной пленки - от 1500 руб.</li>
+                            <li><check-icon></check-icon> Кузовной ремонт - от&nbsp;500&nbsp;руб.</li>
+                            <li><check-icon></check-icon> Локальный ремонт - от&nbsp;3500&nbsp;руб.</li>
+                            <li><check-icon></check-icon> Ремонт без окраса - от&nbsp;1500&nbsp;руб.</li>
+                            <li><check-icon></check-icon> Обработка арок и днища автомобиля антишумом - от&nbsp;2500&nbsp;руб.</li>
+                            <li><check-icon></check-icon> Нанесение защитной пленки - от&nbsp;1500&nbsp;руб.</li>
                         </ul>
                     </template>
                 </badger-accordion-item>
@@ -47,6 +47,7 @@
 </template>
 
 <script>
+    import CheckIcon from '../icons/CheckIcon.vue';
     import {BadgerAccordion, BadgerAccordionItem} from 'vue-badger-accordion';
 
     export default {
@@ -63,14 +64,15 @@
         },
         components: {
             BadgerAccordion,
-            BadgerAccordionItem
+            BadgerAccordionItem,
+            CheckIcon,
         },
         mounted() {
         }
     }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
     .car-services {
         h2 {
             text-align: center;
@@ -87,4 +89,98 @@
             }
         }
     }
+
+    .badger-accordion__header .js-badger-accordion-header .badger-accordion-toggle .badger-toggle-indicator {
+        flex: unset;
+        cursor: pointer;
+        width: 25px;
+        height: 25px;
+        position: relative;
+        color: #fff;
+
+        &:before {
+            content: '';
+            display: block;
+            position: absolute;
+            right: 0;
+            top: 0;
+            background-color: #FF8351;
+            width: 25px;
+            height: 25px;
+            border-radius: 20px;
+        }
+
+        &:after {
+            content: "";
+            display: block;
+            border: 2px solid #ffffff;
+            border-left: none;
+            border-top: none;
+            width: 8px;
+            height: 8px;
+            transform: rotate(45deg);
+            z-index: 1;
+            left: 0;
+            right: 0;
+            top: 6px;
+            margin: 0 auto;
+            position: absolute;
+        }
+    }
+
+    .component-badger-accordion {
+        margin: 0 30px;
+        border-bottom: 1px solid #b7b7b7;
+    }
+
+    @media only screen and (min-width: 700px) {
+        .component-badger-accordion {
+            margin: 0 auto;
+            width: 65%;
+            max-width: 700px;
+        }
+    }
+
+    .badger-accordion__header {
+        .js-badger-accordion-header {
+            .badger-accordion-toggle {
+                font-size: 18px;
+                padding: 12px 5px;
+                border-top: 1px solid #b7b7b7;
+                justify-content: space-between;
+            }
+        }
+    }
+
+    @media only screen and (min-width: 1100px) {
+        .badger-accordion__header {
+            .js-badger-accordion-header {
+                .badger-accordion-toggle {
+                    font-size: 22px;
+                }
+            }
+        }
+    }
+
+    .js-badger-accordion-panel-inner {
+        padding: 20px 5px 20px;
+        ul {
+            li {
+                margin-bottom: 10px;
+                line-height: 1.2;
+                display: flex;
+                align-items: flex-start;
+
+                .check {
+                    width: 16px;
+                    height: 16px;
+                    min-width: 16px;
+                    min-height: 16px;
+                    margin-right: 10px;
+                }
+            }
+        }
+    }
+
+
 </style>
