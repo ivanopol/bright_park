4538c82f1c14d705a2f20b85066c7b5da0508647
ivanopol <ivanopol777@mail.ru>
2020-05-18 18:02:31 +0500

    #service Добавил вывод данных

--- resources/js/components/service/OurServices.vue
+++ resources/js/components/service/OurServices.vue
@@ -2,42 +2,12 @@
     <section class="car-services container">
         <h2><span class="c_orange">Наши</span> услуги</h2>
         <div class="container">
-            <badger-accordion >
-                <badger-accordion-item>
-                    <template slot="header">Техническое обслуживание (ТО)</template>
+            <badger-accordion ref="myAccordion">
+                <badger-accordion-item v-for="service in services" v-bind:key="service.id">
+                    <template slot="header">{{service.title}}</template>
                     <template slot="content">
                         <ul>
-                            <li><check-icon></check-icon> Кузовной ремонт - от&nbsp;500&nbsp;руб.</li>
-                            <li><check-icon></check-icon> Локальный ремонт - от&nbsp;3500&nbsp;руб.</li>
-                            <li><check-icon></check-icon> Ремонт без окраса - от&nbsp;1500&nbsp;руб.</li>
-                            <li><check-icon></check-icon> Обработка арок и днища автомобиля антишумом - от&nbsp;2500&nbsp;руб.</li>
-                            <li><check-icon></check-icon> Нанесение защитной пленки - от&nbsp;1500&nbsp;руб.</li>
-                        </ul>
-                    </template>
-                </badger-accordion-item>
-
-                <badger-accordion-item>
-                    <template slot="header">Кузовной ремонт</template>
-                    <template slot="content">
-                        <ul>
-                            <li><check-icon></check-icon> Кузовной ремонт - от&nbsp;500&nbsp;руб.</li>
-                            <li><check-icon></check-icon> Локальный ремонт - от&nbsp;3500&nbsp;руб.</li>
-                            <li><check-icon></check-icon> Ремонт без окраса - от&nbsp;1500&nbsp;руб.</li>
-                            <li><check-icon></check-icon> Обработка арок и днища автомобиля антишумом - от&nbsp;2500&nbsp;руб.</li>
-                            <li><check-icon></check-icon> Нанесение защитной пленки - от&nbsp;1500&nbsp;руб.</li>
-                        </ul>
-                    </template>
-                </badger-accordion-item>
-
-                <badger-accordion-item>
-                    <template slot="header">Слесарный ремонт</template>
-                    <template slot="content">
-                        <ul>
-                            <li><check-icon></check-icon> Кузовной ремонт - от&nbsp;500&nbsp;руб.</li>
-                            <li><check-icon></check-icon> Локальный ремонт - от&nbsp;3500&nbsp;руб.</li>
-                            <li><check-icon></check-icon> Ремонт без окраса - от&nbsp;1500&nbsp;руб.</li>
-                            <li><check-icon></check-icon> Обработка арок и днища автомобиля антишумом - от&nbsp;2500&nbsp;руб.</li>
-                            <li><check-icon></check-icon> Нанесение защитной пленки - от&nbsp;1500&nbsp;руб.</li>
+                            <li v-for="list in service.list"><check-icon></check-icon> {{list.title}} - от&nbsp;{{list.price}}&nbsp;руб.</li>
                         </ul>
                     </template>
                 </badger-accordion-item>
@@ -55,7 +25,245 @@
         props: {},
         data: function () {
             return {
+                services: {
+                    maintenance: {
+                        id: 1,
+                        title: 'Техническое обслуживание',
+                        list: {
+                            0: {
+                                title: 'Замена масла',
+                                price: 1590,
+                            },
+                            1: {
+                                title: 'Замена фильтра двигателя',
+                                price: 80,
+                            },
+                            2: {
+                                title: 'Замена фильтра салона',
+                                price: 160,
+                            },
+                            3: {
+                                title: 'Замена свечей зажигания',
+                                price: 190,
+                            },
+                            4: {
+                                title: 'Регулировка света фар',
+                                price: 190,
+                            },
+                            5: {
+                                title: 'Замена лампочки',
+                                price: 90,
+                            },
+                        }
+                    },
+                    locksmith_repair: {
+                        id: 2,
+                        title: 'Слесарный ремонт',
+                        list: {
+                            0: {
+                                title: 'Диагностика',
+                                price: 0,
+                            },
+                            1: {
+                                title: 'Сход развал',
+                                price: 1290,
+                            },
+                            2: {
+                                title: 'Замена передних колодок',
+                                price: 790,
+                            },
+                            3: {
+                                title: 'Замена задних колодок',
+                                price: 790,
+                            },
+                            4: {
+                                title: 'Замена передних тормозных дисков',
+                                price: 1290,
+                            },
+                            5: {
+                                title: 'Замена задних тормозных дисков',
+                                price: 1290,
+                            },
+                            6: {
+                                title: 'Замена тормозной жидкости',
+                                price: 790,
+                            },
+                            7: {
+                                title: 'Замена втулок стабилизатора',
+                                price: 690,
+                            },
+                            8: {
+                                title: 'Замена переднего амортизатора',
+                                price: 1090,
+                            },
+                            9: {
+                                title: 'Замена заднего амортизатора',
+                                price: 1090,
+                            },
+                            10: {
+                                title: 'Замена передней балки',
+                                price: 1990,
+                            },
+                            11: {
+                                title: 'Замена подрамника',
+                                price: 2090,
+                            },
+                            12: {
+                                title: 'Замена подшипника ступицы',
+                                price: 1490,
+                            },
+                            13: {
+                                title: 'Замена сайлентблока',
+                                price: 1090,
+                            },
+                            14: {
+                                title: 'Замена стоек стабилизатора',
+                                price: 490,
+                            },
+                            15: {
+                                title: 'Замена ступицы',
+                                price: 1590,
+                            },
+                            16: {
+                                title: 'Замена шаровой опоры',
+                                price: 890,
+                            },
+                            17: {
+                                title: 'Замена рычага',
+                                price: 1190,
+                            },
+                        }
+                    },
+                    body_repair: {
+                        id: 3,
+                        title: 'Кузовной ремонт',
+                        list: {
+                            0: {
+                                title: 'Окраска бампера',
+                                price: 4990,
+                            },
+                            1: {
+                                title: 'Замена лобового стекла',
+                                price: 1990,
+                            },
+                            2: {
+                                title: 'Окраска переднего крыла',
+                                price: 5490,
+                            },
+                            3: {
+                                title: 'Окраска двери',
+                                price: 5490,
+                            },
+                            4: {
+                                title: 'Окраска заднего крыла',
+                                price: 5490,
+                            },
+                            5: {
+                                title: 'Окраска порога',
+                                price: 4490,
+                            },
+                            6: {
+                                title: 'Окраска крышки багажника',
+                                price: 5490,
+                            },
+                            7: {
+                                title: 'Выправление вмятины без окраски',
+                                price: 990,
+                            },
+                            8: {
+                                title: 'Локальная окраска',
+                                price: 2990,
+                            },
+                            9: {
+                                title: 'Бронирование капота',
+                                price: 4990,
+                            },
+                            10: {
+                                title: 'Бронирование фар',
+                                price: 1490,
+                            },
+                            11: {
+                                title: 'Бронирование переднего бампера',
+                                price: 4990,
+                            },
+                            12: {
+                                title: 'Бронирование ниши ручек дверей',
+                                price: 990,
+                            },
+                            13: {
+                                title: 'Бронирование переднего крыла',
+                                price: 2490,
+                            },
+                            14: {
+                                title: 'Бронирование крыши',
+                                price: 4990,
+                            },
+                            15: {
+                                title: 'Бронирование порога',
+                                price: 1990,
+                            },
+                            16: {
+                                title: 'Антикор арки',
+                                price: 490,
+                            },
+                            17: {
+                                title: 'Антикор днища',
+                                price: 1990,
+                            },
+                            18: {
+                                title: 'Окраска панели днища',
+                                price: 4990,
+                            },
+                            19: {
+                                title: 'Окраска капота',
+                                price: 4990,
+                            },
+                            20: {
+                                title: 'Бронирование крыши (полоса РВО)',
+                                price: 990,
+                            },
+                            21: {
+                                title: 'Антикор днища и арки',
+                                price: 3890,
+                            },
+                        }
+                    },
+                    car_care: {
+                        id: 4,
+                        title: 'Уход за автомобилем',
+                        list: {
+                            0: {
+                                title: 'Предпродажная подготовка авто с пробегом',
+                                price: 4900,
+                            },
+                            1: {
+                                title: 'Восстановительная полировка кузова',
+                                price: 7900,
+                            },
+                            2: {
+                                title: 'Керамическое покрытие',
+                                price: 9990,
+                            },
+                            3: {
+                                title: 'Химчистка салона',
+                                price: 2400,
+                            },
+                            4: {
+                                title: 'Легкая полировка фар',
+                                price: 890,
+                            },
+                            5: {
+                                title: 'Легкая полировка кузова',
+                                price: 2400,
+                            },
+                            6: {
+                                title: 'Защитное покрытие кузова',
+                                price: 1990,
+                            },
+                        },
+                    }
 
+                },
             }
         },
         methods: {
@@ -68,6 +276,7 @@
             CheckIcon,
         },
         mounted() {
+            this.$refs.myAccordion.open(0);
         }
     }
 </script>
