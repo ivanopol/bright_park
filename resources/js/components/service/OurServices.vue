df15ac4f04885d0f9517c4d413cd71775e8cfcb6
ivanopol <ivanopol777@mail.ru>
2020-05-18 13:05:08 +0500

    #service Добавил аккордеон vue-badger-accordion

--- resources/js/components/service/OurServices.vue
+++ resources/js/components/service/OurServices.vue
@@ -1,11 +1,54 @@
 <template>
     <section class="car-services container">
         <h2><span class="c_orange">Наши</span> услуги</h2>
+        <div class="container">
+            <badger-accordion>
+                <badger-accordion-item>
+                    <template slot="header">Техническое обслуживание (ТО)</template>
+                    <template slot="content">
+                        <ul>
+                            <li>Кузовной ремонт - от 500 руб.</li>
+                            <li>Локальный ремонт - от 3500 руб.</li>
+                            <li>Ремонт без окраса - от 1500 руб.</li>
+                            <li>Обработка арок и днища автомобиля антишумом - от 2500 руб.</li>
+                            <li>Нанесение защитной пленки - от 1500 руб.</li>
+                        </ul>
+                    </template>
+                </badger-accordion-item>
 
+                <badger-accordion-item>
+                    <template slot="header">Кузовной ремонт</template>
+                    <template slot="content">
+                        <ul>
+                            <li>Кузовной ремонт - от 500 руб.</li>
+                            <li>Локальный ремонт - от 3500 руб.</li>
+                            <li>Ремонт без окраса - от 1500 руб.</li>
+                            <li>Обработка арок и днища автомобиля антишумом - от 2500 руб.</li>
+                            <li>Нанесение защитной пленки - от 1500 руб.</li>
+                        </ul>
+                    </template>
+                </badger-accordion-item>
+
+                <badger-accordion-item>
+                    <template slot="header">Слесарный ремонт</template>
+                    <template slot="content">
+                        <ul>
+                            <li>Кузовной ремонт - от 500 руб.</li>
+                            <li>Локальный ремонт - от 3500 руб.</li>
+                            <li>Ремонт без окраса - от 1500 руб.</li>
+                            <li>Обработка арок и днища автомобиля антишумом - от 2500 руб.</li>
+                            <li>Нанесение защитной пленки - от 1500 руб.</li>
+                        </ul>
+                    </template>
+                </badger-accordion-item>
+            </badger-accordion>
+        </div>
     </section>
 </template>
 
 <script>
+    import {BadgerAccordion, BadgerAccordionItem} from 'vue-badger-accordion';
+
     export default {
         name: "CarServices",
         props: {},
@@ -17,6 +60,10 @@
         methods: {
         },
         computed: {
+        },
+        components: {
+            BadgerAccordion,
+            BadgerAccordionItem
         },
         mounted() {
         }
