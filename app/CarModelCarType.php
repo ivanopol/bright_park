054dd6f3047f2fa5bf4dad79fbe4a2a7d719e2c0
ivanopol <ivanopol777@mail.ru>
2020-06-22 19:24:29 +0500

    #models Добавил поле изображения в объект связи моделей и кузовов

--- app/CarModelCarType.php
+++ app/CarModelCarType.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class CarModelCarType extends Pivot
 {
-    protected $fillable = ['car_model_id', 'car_type_id', 'preview', 'price', 'special_price', 'count'];
+    protected $fillable = ['car_model_id', 'car_type_id', 'preview', 'price', 'special_price', 'image', 'count'];
 
     protected $table = 'car_model_car_type';
 
