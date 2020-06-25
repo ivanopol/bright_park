3516206d33a897316cace4aa87d132fa7c49ab7a
ivanopol <ivanopol777@mail.ru>
2020-06-25 17:45:07 +0500

    #breadcrumbs Исправил ссылку на главную страницу

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -1,13 +1,13 @@
 <?php
 
 // Home
-Breadcrumbs::for('main', function ($trail) {
-    $trail->push('Главная', route('index'));
+Breadcrumbs::for('main', function ($trail, $city) {
+    $trail->push('Главная', route('index', [$city]));
 });
 
 // Models
 Breadcrumbs::for('models', function ($trail, $city, $car_model) {
-    $trail->parent('main');
+    $trail->parent('main', $city);
     $trail->push('Модельный ряд ' . $car_model->title,
         route('models', [$city, $car_model->slug]));
 });
@@ -17,7 +17,7 @@ Breadcrumbs::for('model', function ($trail, $city, $car_model, $car_type) {
 
     $title = $car_model->title === $car_type->title_ru ? $car_model->title : $car_model->title . ' ' . $car_type->title_ru;
 
-    $trail->parent('main');
+    $trail->parent('main', $city);
     $trail->push('Модельный ряд ' . $car_model->title,
         route('models', [$city, $car_model->slug]));
     $trail->push($title,
@@ -29,7 +29,7 @@ Breadcrumbs::for('model_details', function ($trail, $city, $car_model, $car_type
 
     $title = $car_model->title === $car_type->title_ru ? $car_model->title : $car_model->title . ' ' . $car_type->title_ru;
 
-    $trail->parent('main');
+    $trail->parent('main', $city);
     $trail->push('Модельный ряд ' . $car_model->title,
         route('models', [$city, $car_model->slug]));
     $trail->push($title,
@@ -47,35 +47,35 @@ Breadcrumbs::for('contacts', function ($trail, $city) {
 
 // Stocks
 Breadcrumbs::for('stocks', function ($trail, $city) {
-    $trail->parent('main');
+    $trail->parent('main', $city);
     $trail->push('Акции',
         route('stocks', [$city]));
 });
 
 // Stocks one
 Breadcrumbs::for('stocks.one', function ($trail, $city, $stocks) {
-    $trail->parent('main');
+    $trail->parent('main', $city);
     $trail->push('Акции', route('stocks', [$city]));
     $trail->push($stocks->title, route('stocks.one', [$city, $stocks]));
 });
 
 // News
 Breadcrumbs::for('news', function ($trail, $city) {
-    $trail->parent('main');
+    $trail->parent('main', $city);
     $trail->push('Новости',
         route('news', [$city]));
 });
 
 // News one
 Breadcrumbs::for('news.one', function ($trail, $city, $news) {
-    $trail->parent('main');
+    $trail->parent('main', $city);
     $trail->push('Новости', route('news', [$city]));
     $trail->push($news->title, route('news.one', [$city, $news]));
 });
 
 // Privacy
 Breadcrumbs::for('privacy', function ($trail, $city) {
-    $trail->parent('main');
+    $trail->parent('main', $city);
     $trail->push('Политика конфиденциальности', route('privacy', [$city]));
 });
 
