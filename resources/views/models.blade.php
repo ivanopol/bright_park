c18baec7aa595ce62c96eb266fae80def3efa7ff
ivanopol <ivanopol777@mail.ru>
2020-06-25 13:50:41 +0500

    #models Добавил микро-разметку

--- resources/views/models.blade.php
+++ resources/views/models.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div class="">
         <section id="carcasses" class="carcasses_wrap">
-            <div class="container">
+            <div class="container" itemscope itemtype="http://schema.org/Product">
                 <h1>LADA {{$data['carcasses'][0]->title}} от официального дилера в {{$cities['active']['dative']}}</h1>
                 <ul class="carcasses-ul">
                     @foreach ($data['carcasses'][0]->carcasses as $carcass)
@@ -17,19 +17,19 @@
                             <div class="carcasses-list">
                                 <a href="{{ route('model', ['city' => $city, 'car_model' => $data['carcasses'][0]->slug, 'car_type' => $carcass->slug ]) }}">
                                     <div class="carcasses-list-img">
-                                        <img src="{{$carcass->pivot->image}}" alt="{{$data['carcasses'][0]->title . ' ' . $carcass->title_ru}}">
+                                        <img itemprop="image" src="{{$carcass->pivot->image}}" alt="{{$data['carcasses'][0]->title . ' ' . $carcass->title_ru}}">
                                     </div>
-                                    <div class="carcasses-list-description">
+                                    <div class="carcasses-list-description" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
                                         <div class="carcasses-list-title">
-                                            <span class="carcasses-list-title-el">{{$data['carcasses'][0]->title . ' ' . $carcass->title_ru}}</span>
-                                            <span class="carcasses-list-title-slogan">{{$carcass->pivot->slogan}}</span>
+                                            <span class="carcasses-list-title-el" itemprop="name">{{$data['carcasses'][0]->title . ' ' . $carcass->title_ru}}</span>
+                                            <span itemprop="description" class="carcasses-list-title-slogan">{{$carcass->pivot->slogan}}</span>
                                         </div>
-                                        <div class="carcasses-list-prices">
-                                             <span class="carcasses-list-prices-new">от <span>{{ number_format($carcass->pivot->special_price, 0, ',', ' ') }}</span> р.</span>
+                                        <div class="carcasses-list-prices" >
+                                            <span itemprop="price" class="carcasses-list-prices-new">от <span>{{ number_format($carcass->pivot->special_price, 0, ',', ' ') }}</span> <span itemprop="priceCurrency" content="RUB">р.</span></span>
                                              <span class="carcasses-list-prices-old">от <span>{{ number_format($carcass->pivot->price, 0, ',', ' ') }}</span> р.</span>
                                         </div>
                                         <div class="carcasses-list-button">
-                                            <a href="{{ route('model', ['city' => $city, 'car_model' => $data['carcasses'][0]->slug, 'car_type' => $carcass->slug ]) }}" class="btn btn-primary btn-position green">Подробнее</a>
+                                            <a itemprop="availability" href="{{ route('model', ['city' => $city, 'car_model' => $data['carcasses'][0]->slug, 'car_type' => $carcass->slug ]) }}" class="btn btn-primary btn-position green">Подробнее</a>
                                         </div>
                                     </div>
                                 </a>
@@ -37,6 +37,7 @@
                         </li>
                     @endforeach
                 </ul>
+                <div class="text-after"></div>
             </div>
         </section>
         <footer-component :coordinates='@json($data['coordinates'])'
