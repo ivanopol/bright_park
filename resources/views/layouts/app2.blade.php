6c3f62daeeb3e6318975eed5883ed1b08b4e5c73
ivanopol <ivanopol777@mail.ru>
2020-07-03 16:53:14 +0500

    #seo OpenGraph поправил ошибку микроразметки #upd

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# place: http://ogp.me/ns/place#">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Bright Park">
