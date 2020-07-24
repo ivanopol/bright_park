d8d30e24e7f3b14dfc9e621c159dfed648024361
ivanopol <ivanopol777@mail.ru>
2020-07-24 19:02:45 +0500

    #seo Добавил название региона в дательном падеже и названия моделей и кузовов на кириллице

--- database/migrations/2020_01_29_666666_create_car_model_table.php
+++ database/migrations/2020_01_29_666666_create_car_model_table.php
@@ -12,6 +12,7 @@ class CreateCarModelTable extends Migration
             $table->engine = "InnoDB";
             $table->bigIncrements('id');
             $table->string('title');
+            $table->string('title_ru');
             $table->string('slug');
             $table->text('description');
             $table->tinyInteger('sort');
--- database/seeds/CarModelTableSeeder.php
+++ database/seeds/CarModelTableSeeder.php
@@ -11,6 +11,7 @@ class CarModelTableSeeder extends Seeder
         $models = [
             [
                 'title' => 'Granta',
+                'title_ru' => 'Гранта',
                 'slug' => Str::slug('Granta'),
                 'description' => 'Компактный внешне и&nbsp;удивительно просторный внутри. Быстрый, но&nbsp;экономичный. Способный одинаково успешно ездить в&nbsp;любых погодных условиях и&nbsp;в&nbsp;городе, и&nbsp;на&nbsp;трассе. Красивый, комфортный, недорогой и&nbsp;это всё LADA Granta!',
                 'sort' => 1,
@@ -18,6 +19,7 @@ class CarModelTableSeeder extends Seeder
             ],
             [
                 'title' => 'Vesta',
+                'title_ru' => 'Веста',
                 'slug' => Str::slug('Vesta'),
                 'description' => 'LADA Vesta стильная, стремительная, яркая&nbsp;&mdash; такая, каким и&nbsp;должен быть автомобиль LADA нового поколения. Силуэт выдержан в&nbsp;единой &laquo;Икс&raquo;-концепции и&nbsp;выглядит гармонично и&nbsp;современно. С&nbsp;таким автомобилем вряд&nbsp;ли удастся остаться незамеченным на&nbsp;дороге.',
                 'sort' => 2,
@@ -25,6 +27,7 @@ class CarModelTableSeeder extends Seeder
             ],
             [
                 'title' => 'XRAY',
+                'title_ru' => 'Х Рей',
                 'slug' => Str::slug('XRAY'),
                 'description' => 'Компактный&nbsp;&mdash; но&nbsp;солидный, высокий&nbsp;&mdash; но&nbsp;динамичный. LADA XRAY преуспевает во&nbsp;всех дисциплинах: скоростная поездка по&nbsp;трассе, маневрирование в&nbsp;тесноте города, уверенное движение по&nbsp;грунту, снегу и&nbsp;неровностям урбанистического пейзажа. Максимум впечатлений уже ждёт Вас за&nbsp;рулём LADA XRAY!',
                 'sort' => 3,
@@ -32,6 +35,7 @@ class CarModelTableSeeder extends Seeder
             ],
             [
                 'title' => '4x4',
+                'title_ru' => '4x4',
                 'slug' => Str::slug('4x4'),
                 'description' => 'LADA 4&times;4&nbsp;&mdash; легендарный автомобиль, который не&nbsp;может жить без сильных ощущений. Он&nbsp;силен, оборотист и&nbsp;неприхотлив. Его призвание&nbsp;&mdash; бездорожье, но&nbsp;он&nbsp;прекрасно себя чувствует и&nbsp;в&nbsp;городских джунглях. Принципы данной модели: уникальная проходимость, надежность и&nbsp;простота обслуживания.',
                 'sort' => 5,
@@ -39,6 +43,7 @@ class CarModelTableSeeder extends Seeder
             ],
             [
                 'title' => 'Largus',
+                'title_ru' => 'Ларгус',
                 'slug' => Str::slug('Largus'),
                 'description' => 'Этот автомобиль может легко приспособиться к любым вашим потребностям. LADA Largus идеально сочетает в себе комфорт, практичность и высокую полезную нагрузку. Крепкий, сбитый, уверенный Largus однозначно заявляет: «На меня можно положиться!». LADA Largus адаптирован к российским условиям эксплуатации, а отработанная конструкция и технологии обеспечивают высокое качество автомобиля.',
                 'sort' => 4,
@@ -46,6 +51,7 @@ class CarModelTableSeeder extends Seeder
             ],
             [
                 'title' => 'Niva',
+                'title_ru' => 'Нива',
                 'slug' => Str::slug('Niva'),
                 'description' => 'Отправляйтесь в путешествие по горам и городам! Потрясающие внедорожные возможности LADA Niva открывают перед вами все пути.',
                 'sort' => 6,
