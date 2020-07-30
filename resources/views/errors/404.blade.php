e7c3342b04053e9223e168dc4192c37d76f14b6f
FialDarg <SaitoCat228@gmail.com>
2020-07-30 12:52:32 +0500

    404
    Нет стилей, нужно вынести стили во внешний файл, добавить адаптивность

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>404</title>
+    <style>
+        *{
+        margin: 0;
+        padding: 0;
+        }
+        p{
+        margin-top: 10px;
+        }
+        button{
+        margin-top: 10px;
+
+        }
+        .img{
+        text-align: center;
+        margin-top: 300px;
+        }
+    </style>
+</head>
+<body>
+<div class="img">
+    <img src="/build/images/404.svg" alt="404">
+    <p>Извините, страница не найдена</p>
+    <div class="carcasses-list-button"><span class="btn btn-primary btn-position green bubble">Подробнее</span></div>
+</div>
+</body>
+</html>
