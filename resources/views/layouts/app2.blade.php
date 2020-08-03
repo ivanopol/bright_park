b945db3d741abdab5804a61047f60d277e55bd90
ivanopol <ivanopol777@mail.ru>
2020-08-03 15:59:49 +0500

    #optimization Убрал лишнее из файла лэйаута

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Bright Park">
 
-{!! SEO::generate() !!}
+    {!! SEO::generate() !!}
 
-<!-- favicon -->
+    <!-- favicon -->
     <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
@@ -165,65 +165,5 @@
 
 </script>
 @endif
-
-
-    <!-- StreamWood code -->
-   {{-- <link href="https://clients.streamwood.ru/StreamWood/sw.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="https://clients.streamwood.ru/StreamWood/sw.js" charset="utf-8"></script>
-    <script type="text/javascript">
-        swQ(document).ready(function(){
-            swQ().SW({
-                swKey: 'd780aa68ebad661ef6dc3a10ca71b1f5',
-                swDomainKey: 'aff90937246c0f13a056816f279cb551'
-            });
-            swQ('body').SW('load');
-        });
-    </script>
-    <!-- /StreamWood code -->
-
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-        ym(54496129, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-        });
-
-    </script>
-
-    <noscript><div><img src="https://mc.yandex.ru/watch/54496129" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-    <!-- /Yandex.Metrika counter -->
-    <!-- Jivosite -->
-    <script src="//code.jivosite.com/widget/H95FUE1JmR" async></script>
-    <!-- /Jivosite -->
-    <script>
-        function jivo_onLoadCallback(){
-            jivo_api.setUserToken(yaCounter54496129.getClientID());
-        }
-    </script>
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144189432-5"></script>
-
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-144189432-5');
-    </script>
-    <!-- /Global site tag (gtag.js) - Google Analytics -->
-
-    <!-- Calibri -->
-    <script src="//cdn.callibri.ru/callibri.js" type="text/javascript" charset="utf-8" ></script>
-    <script>callibri_wait(window, null, function(){ document.body.dataset.jsLoaded == true}, 10, 50, callibriInit);</script>
-    <!-- /Calibri -->
-    --}}
-
 </body>
 </html>
