0dba2681422978c9ac6a7b42aaafeb9d6285f184
ivanopol <ivanopol777@mail.ru>
2020-11-03 10:54:13 +0500

    #model #plate Изменил дату в плашке

--- resources/js/components/HooperComponent.vue
+++ resources/js/components/HooperComponent.vue
@@ -27,7 +27,7 @@
             </div>
         </div>
         <plate :city="city.value"
-               :text="'До&nbsp;31&nbsp;октября&nbsp;&mdash; акция &laquo;' + action + '&raquo;! Согласуем спец.выгоды, доплатим при обмене'"
+               :text="'До&nbsp;30&nbsp;ноября&nbsp;&mdash; акция &laquo;' + action + '&raquo;! Согласуем спец.выгоды, доплатим при обмене'"
                :disclaimer="'Подробности<span class=\'show_desktop\'> по телефону</span>: <a href=\'tel:' + phone + '\' class=\'callibri_tel event\'>' + phone_formatted + '</a>'"
         ></plate>
     </div>
